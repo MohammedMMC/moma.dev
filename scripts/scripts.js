c8be0739f6db0b1e57d2164c7b90b25cdb0d539e
Mohamed Mahmodi <mohammed5mmc@gmail.com>
2026-01-19 13:39:39 +0300

    added animations to the cards

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,7 +3,13 @@ document.querySelectorAll("section#main .me .links a").forEach((link) => {
   link.addEventListener("mouseover", () => {
     link.style.width = (link.querySelector("span").offsetWidth + 7 + 39 + 5) + "px";
   });
+  link.addEventListener("focus", () => {
+    link.style.width = (link.querySelector("span").offsetWidth + 7 + 39 + 5) + "px";
+  });
   link.addEventListener("mouseout", () => {
+    link.style.width = "39px";
+  });
+  link.addEventListener("blur", () => {
     link.style.width = "39px";
   });
 });
